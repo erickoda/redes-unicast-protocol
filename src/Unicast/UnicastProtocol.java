fd251b2c24c895447179d3244d802016555e59e5
Erick Oda Coulter <erickodac@hotmail.com>
2025-10-17 09:37:01 -0300

    feat: added UDP to UPDataReq

--- src/Unicast/UnicastProtocol.java
+++ src/Unicast/UnicastProtocol.java
@@ -1,5 +1,13 @@
 package src.Unicast;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Optional;
+
+import src.Unicast.Exception.InvalidPDUException;
+
 public class UnicastProtocol implements UnicastServerInterface {
 
     private UnicastServiceUserInterface userService;
@@ -9,6 +17,47 @@ public class UnicastProtocol implements UnicastServerInterface {
 
     @Override
     public boolean UPDataReq(short destination, String message) {
+        int portNumber;
+        UnicastPDU unicastPDU;
+        InetAddress inetAddress;
+        DatagramSocket datagramSocket;
+        DatagramPacket datagramPacket;
+        UnicastAddress destinationAddress;
+        Optional<UnicastAddress> destinationAddressOptional;
+
+        destinationAddressOptional = UnicastAddressSingleton
+                .getUnicastAddressFrom(destination);
+
+        if (destinationAddressOptional.isEmpty()) {
+            System.err.println("[ERROR]: INVALID DESTINATION ADDRESS");
+            return false;
+        }
+
+        destinationAddress = destinationAddressOptional.get();
+
+        try {
+            unicastPDU = new UnicastPDU(message);
+            datagramSocket = new DatagramSocket();
+            inetAddress = InetAddress.getByName(destinationAddress.getHostName());
+            portNumber = destinationAddress.getPortNumber();
+
+            datagramPacket = new DatagramPacket(
+                    unicastPDU.getMessageBytes(),
+                    unicastPDU.getMessageBytes().length,
+                    inetAddress,
+                    portNumber);
+
+            datagramSocket.send(datagramPacket);
+
+            datagramSocket.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+            return false;
+        } catch (InvalidPDUException iPdu) {
+            System.err.println(iPdu);
+            return false;
+        }
+
         return true;
     }
 }
