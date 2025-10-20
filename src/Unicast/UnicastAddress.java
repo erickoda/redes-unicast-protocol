fd83e10e539cb04595c294b19e05141441a564a4
Erick Oda Coulter <erickodac@hotmail.com>
2025-10-20 09:52:50 -0300

    feat: added extra constructor

--- src/Unicast/UnicastAddress.java
+++ src/Unicast/UnicastAddress.java
@@ -5,6 +5,14 @@ public class UnicastAddress {
     private String hostName;
     private int portNumber;
 
+    public UnicastAddress(String line) {
+        String[] words = line.split(" ");
+
+        this.ucsapId = Short.parseShort(words[0]);
+        this.hostName = words[1];
+        this.portNumber = Integer.parseInt(words[2]);
+    }
+
     public UnicastAddress(
             short ucsapId,
             String hostName,
