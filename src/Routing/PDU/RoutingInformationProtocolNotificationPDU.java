fec311b149fe2e8f110d56470305b47b6195ffa5
Erick Oda Coulter <erickodac@hotmail.com>
2025-12-03 08:53:06 -0300

    fix: removed unnecessary fields in rip ntf pdu

--- src/Routing/PDU/RoutingInformationProtocolNotificationPDU.java
+++ src/Routing/PDU/RoutingInformationProtocolNotificationPDU.java
@@ -1,15 +1,9 @@
 package src.Routing.PDU;
 
 public class RoutingInformationProtocolNotificationPDU {
-    private short ripNodeA;
-    private short ripNodeB;
-    private int cost;
     private String message = "";
 
     public RoutingInformationProtocolNotificationPDU(short ripNodeA, short ripNodeB, int cost) {
-        this.ripNodeA = ripNodeA;
-        this.ripNodeB = ripNodeB;
-        this.cost = cost;
         this.message = "RIPNTF " + ripNodeA + " " + ripNodeB + " " + cost;
     }
 
