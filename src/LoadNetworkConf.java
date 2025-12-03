7c30fbd6470cec886695aef603cac7628c39dd6b
Erick Oda Coulter <erickodac@hotmail.com>
2025-12-03 08:53:29 -0300

    fix: removed import

--- src/LoadNetworkConf.java
+++ src/LoadNetworkConf.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class LoadNetworkConf {
